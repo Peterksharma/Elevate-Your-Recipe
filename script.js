168beb1bd3ae22f7be00bc5e6e47df8a1017e2f4
Peter Sharma <peterksharma@gmail.com>
2023-04-10 18:39:54 -0500

    updated JS and got targeting working

--- script.js
+++ script.js
@@ -1,39 +1,44 @@
 function findRecipeList() {
-    const headers = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+  const headers = document.querySelectorAll('h1, h2, h3, h4, h5, h6, page, page-content');
+  console.log('1', headers);
+  
+  for (const header of headers) {
+    const headerText = header.textContent.toLowerCase();
+    console.log('2', headerText);
     
-    for (const header of headers) {
-      const headerText = header.textContent.toLowerCase();
+    if (headerText.includes('ingredients')) {
+      let sibling = header.nextElementSibling;
+      console.log('3', sibling);
       
-      if (headerText.includes('recipe')) {
-        let sibling = header.nextElementSibling;
-        
-        while (sibling) {
-          if (sibling.tagName === 'UL' || sibling.tagName === 'OL') {
-            return sibling.querySelectorAll('li');
-          }
-          
-          sibling = sibling.nextElementSibling;
+      while (sibling) {
+        if (sibling.tagName === 'UL' || sibling.tagName === 'OL') {
+          return sibling.querySelectorAll('li');
         }
         
-        break;
+        sibling = sibling.nextElementSibling;
+        console.log('4', sibling);
       }
+      
+      break;
     }
-    
-    return null;
   }
   
-  // Listen for the message from the popup
-  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === 'findRecipe') {
-      const recipeListItems = findRecipeList();
-  
-      if (recipeListItems) {
-        recipeListItems.forEach((listItem) => {
-          console.log(listItem.textContent);
-        });
-      } else {
-        console.log('Recipe list not found.');
-      }
+  return null;
+}
+
+// Listen for the message from the popup
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === 'findRecipe') {
+    const recipeListItems = findRecipeList();
+    console.log('5', recipeListItems);
+
+    if (recipeListItems) {
+      recipeListItems.forEach((listItem) => {
+        console.log(listItem.textContent);
+        console.log('6');
+      });
+    } else {
+      console.log('Recipe list not found.');
     }
-  });
-  
+  }
+});
