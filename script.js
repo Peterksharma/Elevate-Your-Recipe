c34cda08f6a37bc9fe4a606382245af55811ea33
Peter Sharma <peterksharma@gmail.com>
2023-04-13 12:04:35 -0500

    Created Readme, reworked and commented script.js

--- script.js
+++ script.js
@@ -1,35 +1,45 @@
+
 function findRecipeList() {
-  const headers = document.querySelectorAll('h1, h2, h3, h4, h5, h6, page, page-content');
-  console.log('1', headers);
+  const container = document.querySelector('#wprm-recipe-container-27343');
+  const recipeLists = []; // make an empty array to hold the recipe lists
+
+  if (container) {
+    const lists = container.querySelectorAll('ul, ol');
   
-  for (const header of headers) {
-    const headerText = header.textContent.toLowerCase();
-    console.log('2', headerText);
-    
-    if (headerText.includes('ingredients')) {
-      let sibling = header.nextElementSibling;
-      console.log('3', sibling);
-      
-      while (sibling) {
-        if (sibling.firstChild.tagName === 'UL' || sibling.firstChild.tagName === 'OL') {
-          return sibling.querySelectorAll('li');
-        }
+    lists.forEach(list => {
+      //selecting to lists to search through
+      var ingredientsList = list.querySelectorAll('li');
+      //loop to search the lists looking for criteria and sees if the list contains the proper information or not
+      for (var ingredient of ingredientsList) {
+        var ingredientText = ingredient.textContent.trim();
+        //Looks through lists to find these search credientials. First part searches looking for the whole number or fraction followed by any of the key terms.
+        //  "/d+" looks for a digit, "/s+ searches if there is a space" 
+        // 
+        var searchCredentials = /(\d+\s*(?:\d+\/\d+)?)\s* (cup|cups|oz|ounce|gram|grams|g|teaspoon|tsp|tablespoon|tbsp|)/g; // "/g is global scale"
+        var match = searchCredentials.exec(ingredientText);
         
-        sibling = sibling.nextElementSibling;
-        console.log('4', sibling);
+        if (match) {
+          const quantity = match[1]; // The quantity of the ingredient
+          const unit = match[2]; // The unit of measurement of the ingredient
+          console.log(`Quantity: ${quantity}, Unit: ${unit}`);
+        }
       }
       
-      break;
-    }
+      recipeLists.push(list); // push the list onto the array of recipe lists
+    });
+    
+    return recipeLists; // return the array of recipe lists
+  } else {
+    return null; // return null if the container is not found
   }
-  
-  return null;
 }
+
+
 
 // Listen for the message from the popup
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'findRecipe') {
-    const recipeListItems = findRecipeList();
+    var recipeListItems = findRecipeList();
     console.log('5', recipeListItems);
 
     if (recipeListItems) {
