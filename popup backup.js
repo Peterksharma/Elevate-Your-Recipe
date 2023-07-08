54041dda64e526f36fbc5a9e6e8648311ab07347
Peterksharma <120028593+Peterksharma@users.noreply.github.com>
2023-07-08 18:01:18 -0500

    added service worker in manifest, broke up the recipe Xtract function

--- popup backup.js
+++ popup backup.js
@@ -37,38 +37,34 @@ if (!window.findRecipeListenerAdded) {
   window.findRecipeListenerAdded = true;
 }
 
+// const searchCredentials = /(\d+\s*(?:-\s*\d+)?\s*(?:\d+\/\d+)?)\s*(cup|cups|oz|ounce|ounces|gram|grams|g|teaspoon|tsp|tablespoon|tbsp|lb|lbs|kg|ml|l|pinch|dash|quart|qt|pint|pt|gallon|gal|mg|)/g;
+// //searches for  AP, All Purpose Flour, Bread Flour, Flour, Cake Flour, Pastry Flour, Whole Wheat Flour, and Self Rising Flouw
+// const searchFlour = /(AP|All\s*Purpose|All-Purpose|Bread|Bread\s*Flour|Flour|Cake|Cake\s*Flour|Pastry|Pastry\s*Flour|Whole\s*Wheat|Whole\s*Wheat\s*Flour|Self-Rising|Self-Rising\s*Flour|\w*\s*Flour)/gi;
+// //searches for sugar, granulated sugar, white sugar, brown sugar, light brown sugar, dark brown sugar, powdered sugar, confectioners sugar, cane sugar, raw sugar, turbinado sugar, demerara sugar, muscavado sugar, super fine sugar, coconut sugar, palm sugar, date sugar, maple sugar, baker sugar and with each sugar with a s after the name (baker + bakers sugar)
+// const searchSugar = /(Sugar|Granulated\s*Sugar|White\s*Sugar|Brown\s*Sugar|Light\s*Brown\s*Sugar|Dark\s*Brown\s*Sugar|Powdered\s*Sugar|Confectioners'\s*Sugar|Cane\s*Sugar|Raw\s*Sugar|Turbinado\s*Sugar|Demerara\s*Sugar|Muscovado\s*Sugar|Superfine\s*Sugar|Coconut\s*Sugar|Palm\s*Sugar|Date\s*Sugar|Maple\s*Sugar|Baker\s*Sugar|\w*\s*Sugar)/gi;
 
 function extractRecipe() {
-  //First Part of the Recipe Ingredients Variables (Section the searches the amount numeber)
-  const quantityRegex = /(\d+(?:\.\d+)?(?:\s\d+\/\d+)?|\d+\/\d+)/g;
-  //Second Part of the recipe is the unit for the amount number. 
-  const unitRegex = /(cup|cups|oz|ounce|gram|grams|g|teaspoon|tsp|tablespoon|Tablespoon|tbsp)/g;
-  //Third part Searches through the ingredients to identify the name.
+  const recipeTitle = document.querySelector('h1');
+  const IngredientAmounts = document.querySelectorAll('ul li');
+  const instructionsList = document.querySelectorAll('ol li');
+
+  // Regular expressions for search criteria
   const searchFlour = /(AP|All\s*Purpose|All-Purpose|Bread|Bread\s*Flour|Flour|Cake|Cake\s*Flour|Pastry|Pastry\s*Flour|Whole\s*Wheat|Whole\s*Wheat\s*Flour|Self-Rising|Self-Rising\s*Flour|\w*\s*Flour)/gi;
+  const measurementRegex = /(\d+(?:\.\d+)?(?:\s\d+\/\d+)?|\d+\/\d+)\s*(cup|cups|oz|ounce|gram|grams|g|teaspoon|tsp|tablespoon|Tablespoon|tbsp)/g;
   const searchRiser = /(baking\s*powder|baking\s*soda|yeast)/i;
   const searchLiquid = /(?:warm|cold\s+)?(?:water|water|milk|scalded\s+milk)/i;
   const searchSugar = /(Sugar|Granulated\s*Sugar|White\s*Sugar|Brown\s*Sugar|Light\s*Brown\s*Sugar|Dark\s*Brown\s*Sugar|Powdered\s*Sugar|Confectioners'\s*Sugar|Cane\s*Sugar|Raw\s*Sugar|Turbinado\s*Sugar|Demerara\s*Sugar|Muscovado\s*Sugar|Superfine\s*Sugar|Coconut\s*Sugar|Palm\s*Sugar|Date\s*Sugar|Maple\s*Sugar|Baker\s*Sugar|\w*\s*Sugar)/gi;
 
-  //Find the Recipe Title
-  const recipeTitle = document.querySelector('h1');
   if (recipeTitle) {
     console.log('Recipe Title:', recipeTitle.textContent.trim());
   } else {
     console.error('Recipe title not found.');
   }
-  
-  //Look for the Ingredients
-  const IngredientAmounts = document.querySelectorAll('ul li');
+
   if (IngredientAmounts.length > 0) {
     IngredientAmounts.forEach((ingredient, index) => {
       var ingredientText = ingredient.textContent.trim();
-      var quantityMatch = ingredientText.match(quantityRegex);
-      var unitMatch = ingredientText.match(unitRegex);
-
-      if (quantityMatch && unitMatch && ingredientText.length < 150) {
-        var quantity = quantityMatch[0];  // First group is the quantity
-        var unit = unitMatch[0];  // First group is the unit
-
+      if (measurementRegex.test(ingredientText) && ingredientText.length < 150) {
         searchFlour.lastIndex = 0;
         if (searchFlour.test(ingredientText)) {
           const newAmount = "The Flour amount has been changed. This is the new value...";
@@ -76,12 +72,8 @@ function extractRecipe() {
           const span = document.createElement('span');
           span.className = 'modifiedText';
           span.textContent = newAmount;  // Set the span text to the newAmount
-
-          // Combine quantityRegex and unitRegex for the replacement
-          const combinedRegex = new RegExp(`${quantity}\\s*${unit}`, 'g');
-
           // Replace the old text with the new, styled span
-          ingredient.innerHTML = ingredientText.replace(combinedRegex, span.outerHTML);
+          ingredient.innerHTML = ingredientText.replace(measurementRegex, span.outerHTML);
         }
         searchRiser.lastIndex = 0;
         if (searchRiser.test(ingredientText)) {
@@ -101,8 +93,6 @@ function extractRecipe() {
     console.error('Ingredients not found.');
   }
 
-  //Find the Instructions
-  const instructionsList = document.querySelectorAll('ol li');
   if (instructionsList.length > 0) {
     console.log('Instructions:');
     instructionsList.forEach((instruction, index) => {
--- script.js
+++ script.js
@@ -1,75 +1,35 @@
-
 function findRecipeList() {
-  const container = document.querySelectorAll('body, ol, li, ul');
-  // console.log(container)
-  const recipeLists = []; // make an empty array to hold the recipe lists
+  const lists = document.querySelectorAll('ul, ol');
+  const recipeLists = []; 
 
-  if (container) {
-    const lists = container.querySelectorAll('ul, ol');
-  
-    lists.forEach(list => {
-      //selecting to lists to search through
-      var ingredientsList = list.querySelectorAll('li');
-      //loop to search the lists looking for criteria and sees if the list contains the proper information or not
-      for (var ingredient of ingredientsList) {
-        var ingredientText = ingredient.textContent.trim();
-        console.log("am i working?")
-        //Looks through lists to find these search credientials. First part searches looking for the whole number or fraction followed by any of the key terms.
-        //  "/d+" looks for a digit, "/s+ searches if there is a space" 
-        // looks for units
-        var searchCredentials = /(\d+\s*(?:\d+\/\d+)?)\s* (cup|cups|oz|ounce|gram|grams|g|teaspoon|tsp|tablespoon|tbsp|)/g; // "/g is global scale"
-        var match = searchCredentials.exec(ingredientText);
-        
-        if (match) {
-          const quantity = match[1]; // The quantity of the ingredient
-          const unit = match[2]; // The unit of measurement of the ingredient
-          console.log(`Quantity: ${quantity}, Unit: ${unit}`);
-        }
-      }
+  lists.forEach(list => {
+    var ingredientsList = list.querySelectorAll('li');
+    for (var ingredient of ingredientsList) {
+      var ingredientText = ingredient.textContent.trim();
+      var searchCredentials = /(\d+\s*(?:\d+\/\d+)?)\s*(cup|cups|oz|ounce|gram|grams|g|teaspoon|tsp|tablespoon|tbsp)/g;
+      var match = searchCredentials.exec(ingredientText);
       
-      recipeLists.push(list); // push the list onto the array of recipe lists
-    });
+      if (match) {
+        const quantity = match[1]; 
+        const unit = match[2]; 
+        console.log(`Found ingredient with Quantity: ${quantity}, Unit: ${unit}`);
+      }
+    }
     
-    return recipeLists; // return the array of recipe lists
-  } else {
-    return null; // return null if the container is not found
-  }
+    recipeLists.push(list); 
+  });
+  
+  return recipeLists.length ? recipeLists : null;
 }
 
-
-// Listen for the message from the popup
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  // console.log(request.action);
   if (request.action === 'findRecipe') {
     var recipeListItems = findRecipeList();
-    console.log('5', recipeListItems);
+    console.log('Found Recipe Lists:', recipeListItems);
     
     if (recipeListItems) {
       recipeListItems.forEach(function(listItem) {
-        console.log(listItem.textContent);
-        // localStorage.setItem('Recipe list', JSON.stringify(listItem.textContent));
-        console.log('6');
-      });
-    } else {
-      console.log('Recipe list not found.');
-    }
-  }
-});
-
-
-// Listen for the message from the popup
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  // console.log(request.saction)
-  if (request.action === 'findRecipe') {
-    var recipeListItems = findRecipeList();
-    console.log('5', recipeListItems);
-    
-
-    if (recipeListItems) {
-      recipeListItems.forEach((listItem) => {
-        console.log(listItem.textContent);
-        // localStorage.setItem('Recipe list', JSON.stringify(listItem.textContent));
-        console.log('6');
+        console.log('List Item Text:', listItem.textContent);
       });
     } else {
       console.log('Recipe list not found.');
